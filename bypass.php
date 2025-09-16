3af6c887e9d1ce9d8cfbdad187123c957a384965
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2025-09-17 01:38:10 +0200

    Update bypass.php

--- bypass.php
+++ bypass.php
@@ -6,6 +6,24 @@ function adminer_object() {
         function login($login, $password) {
             return true;
         }
+
+        function credentials() {
+            return array('/tmp/test.db', '', '');
+        }
+
+        function database() {
+            return '/tmp/test.db';
+        }
+
+        function loginForm() {
+            echo '<input type="hidden" name="auth[driver]" value="sqlite">';
+            echo '<input type="hidden" name="auth[server]" value="/tmp/test.db">';
+            echo '<input type="hidden" name="auth[username]" value="">';
+            echo '<input type="hidden" name="auth[password]" value="">';
+            echo '<input type="hidden" name="auth[db]" value="/tmp/test.db">';
+            echo '<p>Connecting to SQLite database...</p>';
+            echo '<script>document.forms[0].submit();</script>';
+        }
     }
 
     return new NoAuthSqlite;
