6a71229d5648c13361094e7d9488f74c815b9142
paoloricciuti <ricciutipaolo@gmail.com>
2025-09-23 18:17:26 +0200

    fix: remove `ts.config`

--- eslint.config.js
+++ eslint.config.js
@@ -10,56 +10,54 @@ import eslint_plugin_import from 'eslint-plugin-import';
 
 const gitignore_path = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
-export default /** @type {ts.Config} */ (
-	ts.config(
-		includeIgnoreFile(gitignore_path),
-		js.configs.recommended,
-		...ts.configs.recommended,
-		...svelte.configs.recommended,
-		eslint_plugin_import.flatConfigs.recommended,
-		prettier,
-		...svelte.configs.prettier,
-		{
-			languageOptions: {
-				globals: { ...globals.browser, ...globals.node },
-			},
-			rules: {
-				// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
-				// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
-				'no-undef': 'off',
-				'@typescript-eslint/naming-convention': [
-					'error',
-					{
-						selector: ['variableLike'],
-						format: ['snake_case', 'UPPER_CASE'],
-						leadingUnderscore: 'allow',
-					},
-				],
-				'func-style': ['error', 'declaration', { allowTypeAnnotation: true }],
-				'import/no-unresolved': 'off', // this doesn't work well with typescript path mapping
-				'import/extensions': [
-					'error',
-					'ignorePackages',
-					{
-						js: 'always',
-						mjs: 'always',
-						cjs: 'always',
-						ts: 'always',
-						svelte: 'always',
-					},
-				],
-			},
+export default /** @type {import("eslint").Linter.Config} */ ([
+	includeIgnoreFile(gitignore_path),
+	js.configs.recommended,
+	...ts.configs.recommended,
+	...svelte.configs.recommended,
+	eslint_plugin_import.flatConfigs.recommended,
+	prettier,
+	...svelte.configs.prettier,
+	{
+		languageOptions: {
+			globals: { ...globals.browser, ...globals.node },
 		},
-		{
-			files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
-			languageOptions: {
-				parserOptions: {
-					projectService: true,
-					extraFileExtensions: ['.svelte'],
-					parser: ts.parser,
-					svelteConfig,
+		rules: {
+			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
+			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+			'no-undef': 'off',
+			'@typescript-eslint/naming-convention': [
+				'error',
+				{
+					selector: ['variableLike'],
+					format: ['snake_case', 'UPPER_CASE'],
+					leadingUnderscore: 'allow',
 				},
+			],
+			'func-style': ['error', 'declaration', { allowTypeAnnotation: true }],
+			'import/no-unresolved': 'off', // this doesn't work well with typescript path mapping
+			'import/extensions': [
+				'error',
+				'ignorePackages',
+				{
+					js: 'always',
+					mjs: 'always',
+					cjs: 'always',
+					ts: 'always',
+					svelte: 'always',
+				},
+			],
+		},
+	},
+	{
+		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+				extraFileExtensions: ['.svelte'],
+				parser: ts.parser,
+				svelteConfig,
 			},
 		},
-	)
-);
+	},
+]);
