5c5de14c6c2f61b4c639ec805038c4321c916eec
jycouet <jycouet@gmail.com>
2025-10-30 22:47:47 +0100

    linting

--- eslint.config.js
+++ eslint.config.js
@@ -23,10 +23,8 @@ export default /** @type {import("eslint").Linter.Config} */ ([
 	...svelte.configs.prettier,
 	{
 		languageOptions: {
-			parser: ts.parser,
 			globals: { ...globals.browser, ...globals.node },
 		},
-
 		rules: {
 			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
 			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
